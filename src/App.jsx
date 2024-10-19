42365db6995c5e00adea83adddcab22a9993c97b
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-10-19 02:03:35 -0300

    [feat] First steps into stylization
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [userInput, setUserInput] = useState("");
+
+    const handleSendMessageBtn = () => {
+        if(input.value == "" || input.value == null) return;
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="page">
+            <Header />
+
+            <article className='chatbot-container'>
+                <section className='chat-messages-container'>
+                    <p className="message der-message">Sou o Der</p>
+                    <p className="message user-message">Sou o User</p>
+                </section>
+
+                <section className='input-container'>
+                    <input
+                        type="text"
+                        name='user-input'
+                        className='user-input'
+                        id='user-input'
+                        placeholder='Fala com o DER aí'
+                        autoFocus={true}
+                        value={userInput}
+                        onChange={(event) => setUserInput(event.target.value)}
+                    />
+                    
+                    <button 
+                        id="user-input-btn"
+                        onClick={handleSendMessageBtn}
+                    >
+                        &gt;
+                    </button>
+                </section>
+            </article>
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import '../../styles/Header/Header.css'
+
+function Header() {
+    return (
+        <>
+            <nav className='glass-effect'>Der Agropesca</nav>
+        </>
+    );
+}
+
+export default Header;
