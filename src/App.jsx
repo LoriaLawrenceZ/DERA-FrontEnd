4c1d2170c810b40450f068f582ccc5d36a3614f6
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-10-20 00:23:06 -0300

    [feat] Setting up final steps of der
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
             },
             body: JSON.stringify({ msg: userMessage }),
         });
