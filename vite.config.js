04aff3731964e06bae36ef1bcda621372d25f8a1
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-10-19 22:39:06 -0300

    [chore] Configuring CORS to allow dera-frontend
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/chat': {
+        target: 'https://dera-backend.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
